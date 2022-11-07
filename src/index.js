a3b537fafcfcff4f2f37ff7810feefacd1e87ac7
Jennifer Holcomb <jenniferlholcomb@gmail.com>
2022-11-07 15:12:12 -0800

    Journal method for vowels method.
    Co-authored-by: VW <vera.weikel@gmail.com>

--- src/index.js
+++ src/index.js
@@ -9,8 +9,11 @@ function handleJournalForm() {
   const length2 = parseInt(document.querySelector('#length2').value);
   const length1 = parseInt(document.querySelector('#length1').value);
   const length3 = parseInt(document.querySelector('#length3').value);
-  const journal = new Journal(length1, length2, length3);
-  const response = journal.checkType();
+  const journal = new Journal("A cat", "My cat is fluffy, and small.");
+  const numChar = journal.numberOfCharacters();
+  console.log(numChar);
+  const numVowels = journal.vowels(numChar);
+  console.log(numVowels);
   const pTag = document.createElement("p");
   pTag.append(response);
   document.querySelector('#response').append(pTag);
--- src/journal.js
+++ src/journal.js
@@ -6,4 +6,26 @@ export default function Journal(title, entryBody) {
 Journal.prototype.numberOfCharacters = function() {
   const charCount = this.entryBody.length;
   console.log(charCount);
+  return charCount;
 };
+
+Journal.prototype.vowels = function (charCount) {
+  const vowelSearch = 0;
+  console.log(vowelSearch);
+  for(let i = 0; i<= charCount; i++){
+    if(this.entryBody.charAt(i) === 'a'){
+      vowelSearch++;
+    } else if ( this.entryBody.charAt(i) ==='e') {
+      vowelSearch++;
+    } else if ( this.entryBody.charAt(i) ==='i'){
+      vowelSearch++;
+    } else if ( this.entryBody.charAt(i) ==='o') {
+        vowelSearch++;
+    } else if ( this.entryBody.charAt(i) ==='u') {
+      vowelSearch++;
+    }
+    console.log(vowelSearch);
+    return vowelSearch;
+  }
+}
+
