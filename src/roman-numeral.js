14305d12992721dfe99cd6ce3b13650701ed91e2
Jennifer Holcomb <jenniferlholcomb@gmail.com>
2022-11-08 14:16:38 -0800

    Test to create numbers property in RomanNumerals object passed.
    Co-authored-by: Henry Sullivan <sullivanbhenry@gmail.com>

--- src/roman-numeral.js
+++ src/roman-numeral.js
@@ -1,13 +1,14 @@
 export default function RomanNumerals() {
-  this.symbols = ['I', 'V', 'X', 'L', 'C', 'D', 'M']
+  this.symbols = ['I', 'V', 'X', 'L', 'C', 'D', 'M'];
+  this.numbers = [];
 }
 
 RomanNumerals.prototype.convertNum = function(inputNum) {
  if(inputNum>3999) {
   console.log(inputNum);
-  return "please enter a number between 1 and 3999"
+  return "please enter a number between 1 and 3999";
  }else {
-  return "good number selection"
+  return "good number selection";
  }
 };
 
