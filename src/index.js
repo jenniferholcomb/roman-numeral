88238898da5c16a917c81af945b9c726550f637e
Jennifer Holcomb <jenniferlholcomb@gmail.com>
2022-11-07 16:05:48 -0800

    Vowels method working, with input values from user in UI logic.
    Co-authored-by: VW <vera.weikel@gmail.com>

--- src/index.js
+++ src/index.js
@@ -6,17 +6,16 @@ import './css/styles.css';
 function handleJournalForm() {
   event.preventDefault();
   document.querySelector('#response').innerText = null;
-  const length2 = parseInt(document.querySelector('#length2').value);
-  const length1 = parseInt(document.querySelector('#length1').value);
-  const length3 = parseInt(document.querySelector('#length3').value);
-  const journal = new Journal("A cat", "My cat is fluffy, and small.");
+  const input1 = document.querySelector('#input1').value;
+  const input2 = document.querySelector('#input2').value;
+  const journal = new Journal(input1, input2);
   const numChar = journal.numberOfCharacters();
-  console.log(numChar);
-  const numVowels = journal.vowels(numChar);
-  console.log(numVowels);
-  const pTag = document.createElement("p");
-  pTag.append(response);
-  document.querySelector('#response').append(pTag);
+  console.log("numChar: ", numChar);
+  const numVowels = journal.vowels();
+  console.log("numVowels: ", numVowels);
+  //const pTag = document.createElement("p");
+  //pTag.append(response);
+  //document.querySelector('#response').append(pTag);
 }
 
 window.addEventListener("load", function() {
--- src/journal.js
+++ src/journal.js
@@ -1,31 +1,34 @@
-export default function Journal(title, entryBody) {
-  this.title = title;
-  this.entryBody = entryBody;
+export default function Journal(input1, input2) {
+  this.input1 = input1;
+  this.input2 = input2;
 }
 
 Journal.prototype.numberOfCharacters = function() {
-  const charCount = this.entryBody.length;
+  const charCount = this.input2.length;
   console.log(charCount);
   return charCount;
 };
 
-Journal.prototype.vowels = function (charCount) {
-  const vowelSearch = 0;
-  console.log(vowelSearch);
-  for(let i = 0; i<= charCount; i++){
-    if(this.entryBody.charAt(i) === 'a'){
+Journal.prototype.vowels = function () {
+  let vowelSearch = 0;
+  const bodyString = this.input2;
+  console.log(bodyString.charAt(4));
+  for(let i = 0; i<= bodyString.length; i++){
+    console.log("I'm here");
+    if(bodyString.charAt(i) === 'a'){
+      console.log("Found an i");
       vowelSearch++;
-    } else if ( this.entryBody.charAt(i) ==='e') {
+    } else if ( this.input2.charAt(i) ==='e') {
       vowelSearch++;
-    } else if ( this.entryBody.charAt(i) ==='i'){
+    } else if ( this.input2.charAt(i) ==='i'){
       vowelSearch++;
-    } else if ( this.entryBody.charAt(i) ==='o') {
+    } else if ( this.input2.charAt(i) ==='o') {
         vowelSearch++;
-    } else if ( this.entryBody.charAt(i) ==='u') {
+    } else if ( this.input2.charAt(i) ==='u') {
       vowelSearch++;
     }
-    console.log(vowelSearch);
-    return vowelSearch;
   }
+  console.log("We're out");
+  return vowelSearch;
 }
 
