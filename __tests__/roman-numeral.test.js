835bb09bca87d2d85b35f248a2bafeeead293c51
Jennifer Holcomb <jenniferlholcomb@gmail.com>
2022-11-08 13:48:24 -0800

    Pass converNum prototype method test.
    Co-authored-by: Henry Sullivan <sullivanbhenry@gmail.com>

--- __tests__/roman-numeral.test.js
+++ __tests__/roman-numeral.test.js
@@ -6,4 +6,11 @@ describe('RomanNumerals', () => {
     const romanNum = new RomanNumerals();
     expect(romanNum.symbols).toEqual(['I', 'V', 'X', 'L', 'C', 'D', 'M'])
   })
+
+  test('should return an error message if number is greater than 3999', () => {
+    const romanNum = new RomanNumerals();
+    expect(romanNum.convertNum(4000)).toEqual("please enter a number between 1 and 3999");
+    expect(romanNum.convertNum(3000)).toEqual("good number selection");
+  })
+
 })
--- src/roman-numeral.js
+++ src/roman-numeral.js
@@ -2,8 +2,11 @@ export default function RomanNumerals() {
   this.symbols = ['I', 'V', 'X', 'L', 'C', 'D', 'M']
 }
 
-// Journal.prototype.numberOfCharacters = function() {
-//   const charCount = this.input2.length;
-//   console.log(charCount);
-//   return charCount;
-// };
+RomanNumerals.prototype.convertNum = function(inputNum) {
+ if(inputNum>3999) {
+  console.log(inputNum);
+  return "please enter a number between 1 and 3999"
+ }else {
+  return "good number selection"
+ }
+};
